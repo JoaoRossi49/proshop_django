3033cc761d4a76a2e9235fe95cabee74bb94ffda
JoaoRossi49 <joao.rossi.figueiredo@gmail.com>
2023-09-02 13:19:58 -0300

    Product listing

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import {Row, Col} from 'react-bootstrap'
+import products from '../products'
 
 function HomeScreen() {
   return (
     <div>
-      <h1>Latest Products</h1>
+      <h1>Listagem de produtos</h1>
+      <Row>
+        {products.map(product => (
+            <Col>
+                <h3>{product.name}</h3>
+            </Col>
+        ))}
+      </Row>
     </div>
   )
 }
