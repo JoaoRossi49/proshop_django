050963a410c0d8c1504fb1c2b52276a3d03eef2d
JoaoRossi49 <joao.rossi.figueiredo@gmail.com>
2023-10-01 17:01:38 -0300

    Homescreen, product cards and ratings

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Row, Col} from 'react-bootstrap'
 import products from '../products'
+import Product from '../components/Product'
 
 function HomeScreen() {
   return (
@@ -8,8 +9,8 @@ function HomeScreen() {
       <h1>Listagem de produtos</h1>
       <Row>
         {products.map(product => (
-            <Col>
-                <h3>{product.name}</h3>
+            <Col key={product.id} sm= {12} md = {6} Lg= {4} xL = {3}>
+                <Product product={product}/>
             </Col>
         ))}
       </Row>
